55298f896113cddbca36a5eaf562cd30fd78f9ca
kim_Herrera <ipurpleyoumin2@gmail.com>
2024-12-13 19:42:43 -0500

    Update footer.php

---
+++ app/views/inc/footer.php
@@ -0,0 +1,34 @@
+<section id="Footer">
+    <div class="container pt-5 py-3">
+        <footer class="py-0 my-0">
+            <ul class="nav justify-content-center border-bottom pb-2 mb-3">
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-3 fs-6 link-hover">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-3 fs-6 link-hover">Servicios</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-3 fs-6 link-hover">Contáctanos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link px-3 fs-6 link-hover">Nosotros</a>
+                </li>
+            </ul>
+            <p class="text-center fs-6 link-hover">&copy; 2024 Company, Inc</p>
+        </footer>
+    </div>
+</section>
+
+<style>
+    .link-hover {
+        color: #6c757d;
+        transition: all 0.3s ease;
+    }
+
+    .link-hover:hover {
+        color: #000000;
+        font-size: 1.5rem;
+        transform: scale(1.2);
+    }
+</style>
